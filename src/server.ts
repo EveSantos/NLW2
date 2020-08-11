c8cb25c6baab3df6a4c52e801fe7c4951c385be0
EveSantos <vrevyn@gmail.com>
2020-08-11 17:45:49 -0300

    :wolf: criando migrations

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.delete('/users/:id',(request, response ) =>{
+    console.log(request.params);
+
+    const users = [
+        { name: 'Diego', age: 25},
+        { name: 'Vini', age: 21},
+
+    ];
+
+    return response.json(users);
+});
+
+app.listen(3333);
